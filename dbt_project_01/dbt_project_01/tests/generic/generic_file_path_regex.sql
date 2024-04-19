94520112676c9eae05f497fa796fd789a9b1f8fa
leonardo martin <leonardo.martin@hiscox.com>
2024-04-19 11:24:43 -0300

    second improve reuse generic_regex.sql

--- dbt_project_01/dbt_project_01/tests/generic/generic_file_path_regex.sql
+++
@@ -1,8 +0,0 @@
-generic_file_path_regex.sql
-{% test generic_file_path_regex(model, column_name) %}
-
-    select {{ column_name }}
-    from {{ model }}
-    where not {{ column_name }} ~*'^abfss:\/\/webmethods-[a-z0-9@\.]+dfs\.core\.windows\.net\/mustang\/regression\/\d{4}\/\d{2}\/\d{2}\/tns_mustang_regression_\d{9}_\d{14}$'
-
-{% endtest %}
---
+++ dbt_project_01/dbt_project_01/tests/generic/generic_regex.sql
@@ -0,0 +1,8 @@
+generic_regex.sql
+{% test generic_regex(model, column_name, regex) %}
+
+    select {{ column_name }}
+    from {{ model }}
+    where not {{ column_name }} ~* '{{ regex }}'
+
+{% endtest %}
